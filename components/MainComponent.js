d90ee667f3b55b5b05a3027411e791cd397ecf78
Susie <susie09rob@gmail.com>
2020-11-17 23:04:03 -0800

    Navigation Part 3

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -8,66 +8,67 @@ import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 
 const DirectoryNavigator = createStackNavigator(
-    {
-        Directory: { screen: Directory },
-        CampsiteInfo: { screen: CampsiteInfo }
-    }, 
-    {
-        initialRouteName: 'Directory',
-        defaultNavigationOptions: {
-            headerStyle: {
-                backgroundColor: '#5637DD'
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-                color: '#fff'
-            }
-        }
-    }
+	{
+		Directory: { screen: Directory },
+		CampsiteInfo: { screen: CampsiteInfo },
+	},
+	{
+		initialRouteName: 'Directory',
+		defaultNavigationOptions: {
+			headerStyle: {
+				backgroundColor: '#5637DD',
+			},
+			headerTintColor: '#fff',
+			headerTitleStyle: {
+				color: '#fff',
+			},
+		},
+	}
 );
 
 const HomeNavigator = createStackNavigator(
-    {
-        Home: { screen: Home }
-    }, 
-    {
-        defaultNavigationOptions: {
-            headerStyle: {
-                backgroundColor: '#5637DD'
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-                color: '#fff'
-            }
-        }
-    }
+	{
+		Home: { screen: Home },
+	},
+	{
+		defaultNavigationOptions: {
+			headerStyle: {
+				backgroundColor: '#5637DD',
+			},
+			headerTintColor: '#fff',
+			headerTitleStyle: {
+				color: '#fff',
+			},
+		},
+	}
 );
 
 const MainNavigator = createDrawerNavigator(
-    {
-        Home: {screen: HomeNavigator },       //object that contains screens in the drawer. Wrap them through stack navigator
-        Directory: {screen: DirectoryNavigator}
-    },
-    {
-        drawerBackgroundColor: '#CEC8FF'   //optional second argument for additional configuration, set drawer bg color.
-    }
-)
+	{
+		Home: { screen: HomeNavigator }, //object that contains screens in the drawer. Wrap them through stack navigator
+		Directory: { screen: DirectoryNavigator },
+	},
+	{
+		drawerBackgroundColor: '#CEC8FF', //optional second argument for additional configuration, set drawer bg color.
+	}
+);
 
 const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
-    render() {
-        return (
-            <View 
-                style=
-                    {{flex: 1,
-                        paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight   
-                }}
-            >
-                <AppNavigator />
-            </View>
-        );
-    }
+	render() {
+		return (
+			<View
+				style={{
+					flex: 1,
+					paddingTop:
+						Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight,
+				}}
+			>
+				<AppNavigator />
+			</View>
+		);
+	}
 }
 
 export default Main;
