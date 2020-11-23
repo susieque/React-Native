b92935234ba66c902785fdb17c43e4b64378e275
Susie <susie09rob@gmail.com>
2020-11-23 14:17:32 -0800

    Navigation Icons and Custom Side Drawer

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,15 +4,27 @@ import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
-import { View, Platform } from 'react-native';
+import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
-import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
+import { Icon } from 'react-native-elements';
+import SafeAreaView from 'react-native-safe-area-view';
 
 const DirectoryNavigator = createStackNavigator(
 	{
-		Directory: { screen: Directory },
-		CampsiteInfo: { screen: CampsiteInfo },
+		Directory: { 
+			screen: Directory,
+			navigationOptions: ({navigation}) => ({
+				headerLeft: <Icon
+				name='list'
+				type='font-awesome'
+				iconStyle={styles.stackIcon}
+				onPress={() => navigation.toggleDrawer()}
+				/>
+			})
+		},
+		CampsiteInfo: { screen: CampsiteInfo }
 	},
 	{
 		initialRouteName: 'Directory',
@@ -33,7 +45,7 @@ const HomeNavigator = createStackNavigator(
 		Home: { screen: Home },
 	},
 	{
-		defaultNavigationOptions: {
+		defaultNavigationOptions: ({navigation}) => ({    //wrap the object in parentheses so arrow function doesn't get confused. Might think that's beginning curly brace for function body. Its beginning curly brace for object literal so it needs parenthesis. Dont for at bottom too.
 			headerStyle: {
 				backgroundColor: '#5637DD',
 			},
@@ -41,7 +53,13 @@ const HomeNavigator = createStackNavigator(
 			headerTitleStyle: {
 				color: '#fff',
 			},
-		},
+			headerLeft: <Icon
+				name='home'
+				type='font-awesome'
+				iconStyle={styles.stackIcon}
+				onPress={() => navigation.toggleDrawer()}
+			/>			
+		})
 	}
 );
 
@@ -50,7 +68,7 @@ const AboutNavigator = createStackNavigator(
 		About: { screen: About },
 	},
 	{
-		defaultNavigationOptions: {
+		defaultNavigationOptions: ({navigation}) => ({    //wrap the object in parentheses so arrow function doesn't get confused. Might think that's beginning curly brace for function body. Its beginning curly brace for object literal so it needs parenthesis. Dont for at bottom too.
 			headerStyle: {
 				backgroundColor: '#5637DD',
 			},
@@ -58,8 +76,14 @@ const AboutNavigator = createStackNavigator(
 			headerTitleStyle: {
 				color: '#fff',
 			},
-		},
-	}
+			headerLeft: <Icon
+				name='info-circle'
+				type='font-awesome'
+				iconStyle={styles.stackIcon}
+				onPress={() => navigation.toggleDrawer()}
+			/>			
+		})
+	}	
 );
 
 const ContactNavigator = createStackNavigator(
@@ -67,7 +91,7 @@ const ContactNavigator = createStackNavigator(
 		Contact: { screen: Contact },
 	},
 	{
-		defaultNavigationOptions: {
+		defaultNavigationOptions: ({navigation}) => ({    //wrap the object in parentheses so arrow function doesn't get confused. Might think that's beginning curly brace for function body. Its beginning curly brace for object literal so it needs parenthesis. Dont for at bottom too.
 			headerStyle: {
 				backgroundColor: '#5637DD',
 			},
@@ -75,19 +99,99 @@ const ContactNavigator = createStackNavigator(
 			headerTitleStyle: {
 				color: '#fff',
 			},
-		},
+			headerLeft: <Icon
+				name='address-card'
+				type='font-awesome'
+				iconStyle={styles.stackIcon}
+				onPress={() => navigation.toggleDrawer()}
+			/>			
+		})
 	}
+);
+
+const CustomDrawerContentComponent = props => (  //SafeAreaView specifically for iphone X. Defines part of area as safe area where nothing else will be laid out. to account for physical layout of iphone X rounded corners and camera notch. Default includes this but we're overwriting it w/custom component, we have to add it. 
+	<ScrollView>
+		<SafeAreaView
+			style={styles.container}
+			forceInset={{top: 'always', horizonal: 'never'}}
+		>  
+			<View style={styles.drawerHeader}> 
+				<View style={{flex: 1}}>  
+					<Image
+						source={require('./images/logo.png')}
+						style={styles.drawerImage}
+					/>						
+				</View>
+				<View style={{flex: 2}}>
+					<Text style={styles.drawerHeaderText}>NuCamp</Text>
+				</View>
+			</View>  
+			<DrawerItems { ...props} />
+		</SafeAreaView>
+	</ScrollView>
+
 );
 
 const MainNavigator = createDrawerNavigator(
 	{
-		Home: { screen: HomeNavigator }, //object that contains screens in the drawer. Wrap them through stack navigator
-		Directory: { screen: DirectoryNavigator },
-		About: { screen: AboutNavigator },
-		Contact: { screen: ContactNavigator }	
+		Home: { 
+			screen: HomeNavigator,
+			navigationOptions: {
+				drawerIcon: ({tintColor}) => (
+					<Icon
+						name='home'
+						type='font-awesome'
+						size={24}
+						color={tintColor}
+					/>
+				)
+			}
+		}, 
+		Directory: { 
+			screen: DirectoryNavigator, 
+			navigationOptions: {
+				drawerIcon: ({tintColor}) => (
+					<Icon
+						name='list'
+						type='font-awesome'
+						size={24}
+						color={tintColor}
+					/>
+				)
+			}
+		},
+		About: {
+			screen: AboutNavigator, 
+			navigationOptions: {
+				drawerLabel: 'About Us',
+				drawerIcon: ({tintColor}) => (
+					<Icon
+						name='info-circle'
+						type='font-awesome'
+						size={24}
+						color={tintColor}
+					/>
+				)
+			}
+		},
+		Contact: {
+			screen: ContactNavigator,
+			navigationOptions: {
+				drawerLabel: 'Contact Us',
+				drawerIcon: ({tintColor}) => (
+					<Icon
+						name='address-card'
+						type='font-awesome'
+						size={24}
+						color={tintColor}
+					/>
+				)
+			}
+		}	
 	},
 	{
 		drawerBackgroundColor: '#CEC8FF', //optional second argument for additional configuration, set drawer bg color.
+		contentComponent: CustomDrawerContentComponent
 	}
 );
 
@@ -108,5 +212,33 @@ class Main extends Component {
 		);
 	}
 }
+const styles= StyleSheet.create ({
+	container: {
+		flex: 1
+	},
+	drawerHeader: {
+		backgroundColor: '#5637DD',
+		height: 140,
+		alignItems: 'center',
+		justifyContent: 'center',
+		flex: 1,
+		flexDirection: 'row'
+	},
+	drawerHeaderText: {
+		color: '#fff',
+		fontSize: 24,
+		fontWeight: 'bold'
+	},
+	drawerImage: {
+		margin: 10,
+		height: 60,
+		width:60
+	},
+	stackIcon: {
+		marginLeft: 10,
+		color: '#fff',
+		fontSize: 24
+	}
+});
 
 export default Main;
