12f1391db5c5ce533eef064bf597ffe0afdfb454
Susie <susie09rob@gmail.com>
2020-11-23 17:33:17 -0800

    Setting up Redux

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { ScrollView, Text } from 'react-native';
 import { Card } from 'react-native-elements';
-import { Promotions } from '../shared/promotions';
 
 
 class Contact extends Component {
