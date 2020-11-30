7b65aeaebf2a3772a4c152bf30071a42b74f6208
Susie <susie09rob@gmail.com>
2020-11-30 13:42:39 -0800

    Swipe and Delete Favorites

--- redux/favorites.js
+++ redux/favorites.js
@@ -8,6 +8,10 @@ export const favorites = (state = [], action) => {
 			}
 			return state.concat(action.payload); //if campsite id doesn't exist in array, returns new state with new fav campsite id concatenated to the end of it.
 		//concat makes copy of array that its acting upon, adds new item at end, returns new array w/o mutating previous array.
+		
+		case ActionTypes.DELETE_FAVORITE:
+			return state.filter(favorite => favorite !== action.payload); //by filtering into it every campsite that doesn't match the campsiteId in the action payload. So that creates a new array that no longer contains the campsiteId being deleted. And can return that as a new state.
+
 		default:
 			return state; //returns previous state
 	}
