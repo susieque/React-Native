ee88658f43c0d4abf03b024be0f536a986d6d90c
Susie <susie09rob@gmail.com>
2020-12-03 01:05:59 -0800

    Persist Redux Store

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 // import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Main from './components/MainComponent';
-import { Provider } from 'react-redux';   
-import { ConfigureStore } from './redux/configureStore';  //creates, configures and returns the redux store
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore'; //creates, configures and returns the redux store
+import { PersistGate } from 'redux-persist/es/integration/react';
+import Loading from './components/LoadingComponent';
 
- const store = ConfigureStore();  //create redux store here.
+const { persistor, store } = ConfigureStore(); //create redux store here.
 
 export default function App() {
-  return (                    //wrap Main component inside Provider component, passing store to provider component as a prop. Gives Main and its child components ability to connect to redux store.
-    <Provider store={store}>  
-        <Main /> 
-    </Provider>
-  );
+	return (
+		//wrap Main component inside Provider component, passing store to provider component as a prop. Gives Main and its child components ability to connect to redux store.
+		<Provider store={store}>
+			<PersistGate loading={<Loading />} persistor={persistor}>
+				<Main />
+			</PersistGate>
+		</Provider>
+	);
 }
 // It's prepared to connect to redux store, but go into each component to connect
-
--- redux/configureStore.js
+++ redux/configureStore.js
@@ -6,10 +6,18 @@ import { comments } from './comments';
 import { promotions } from './promotions';
 import { partners } from './partners';
 import { favorites } from './favorites';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/es/storage';
+
+const config = {
+    key: 'root',
+    storage,  //local storage
+    debug: true
+}
 
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
+        persistCombineReducers(config, {
             campsites,
             comments,
             partners,
@@ -19,5 +27,6 @@ export const ConfigureStore = () => {
         applyMiddleware(thunk, logger)
     );
 
-    return store;
-}
+    const persistor = persistStore(store); //redux store as an argument. Enables store to be persisited. Will use this variable in app.js 
+    return { persistor, store }; //to access both from app.js
+};
