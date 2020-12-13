1f55be49057b223be6300c2c2c038791511c397b
Susie <susie09rob@gmail.com>
2020-12-12 17:21:42 -0800

    React-Native final assignment

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -4,6 +4,7 @@ import { Input, CheckBox, Button, Icon } from 'react-native-elements';
 import * as SecureStore from 'expo-secure-store';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
+import * as ImageManipulator from 'expo-image-manipulator';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { baseUrl } from '../shared/baseUrl';
 
@@ -159,9 +160,35 @@ class RegisterTab extends Component {
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
                 this.setState({imageUrl: capturedImage.uri});
+                this.processImage(capturedImage.uri);
             }
         }
     }
+
+    processImage = async (imgUri) => {
+        const processedImage = await ImageManipulator.manipulateAsync(
+            imgUri,
+            [{resize:{width: 400}}],
+            {format: "png"},
+        );
+        console.log(processedImage);
+        this.setState({imageUrl: processedImage.uri});
+    };
+
+    getImageFromGallery = async () => {
+        const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+       
+        if (cameraRollPermission.status === 'granted') {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1]
+            });
+            if (!capturedImage.cancelled) {
+                console.log(capturedImage);
+                this.processImage(capturedImage.uri);
+            }
+        }
+    };
 
     handleRegister() {
         console.log(JSON.stringify(this.state));
@@ -189,6 +216,10 @@ class RegisterTab extends Component {
                         <Button
                             title='Camera'
                             onPress={this.getImageFromCamera}
+                        />
+                        <Button
+                            title='Gallery'
+                            onPress={this.getImageFromGallery}
                         />
                     </View>
                     <Input
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -339,23 +339,27 @@ class Main extends Component {
 		this.props.fetchPromotions();
 		this.props.fetchPartners();
 
-		NetInfo.fetch().then((connectionInfo) => {
-			(Platform.OS === 'ios')
-				? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
-				: ToastAndroid.show(
-						'Initial Network Connectivity Type: ' + connectionInfo.type,
-						ToastAndroid.LONG
-				  );
-		});
+		this.showNetInfo();
 
-		this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
-			this.handleConnectivityChange(connectionInfo);
+		this.unscubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
+			this.handleConnectivitiyChange(connectionInfo);
 		});
 	}
 
 	componentWillUnmount() {
 		this.unsubscribeNetInfo();
 	}
+
+		showNetInfo = async () => {
+		let connectionInfo = await NetInfo.fetch()
+			(Platform.OS === 'ios')
+				? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+				: ToastAndroid.show(
+						'Initial Network Connectivity Type: ' + connectionInfo.type,
+						ToastAndroid.LONG
+				  );
+		}
+	
 
 	handleConnectivityChange = connectionInfo => {
 		let connectionMsg = 'You are now connected to an active network.';
